f871d99d46d576f6c927fcfe40d8ddaf8613e628
Sand Despotism <wowow3722123@gmail.com>
2024-02-25 13:49:58 +0300

    fixed html send

---
+++ index.py
@@ -0,0 +1,48 @@
+text = """<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SPAM</title>
+  </head>
+  <body
+    style="
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      align-content: center;
+    "
+  >
+    <div
+      style="
+        display: flex;
+        flex-direction: column;
+        width: 600px;
+        min-height: 100vh;
+      "
+    >
+      <h1>Уважаемый Евгений Генадьевич!</h1>
+      <p>
+        Здравствуйте! Вам необходимо прибыть во Второй отдел МИЭТ (ауд.3239Б) в
+        часы приема: с 9-00 до 12-00 и с 14-00 до 17-00 в рабочие дни, при себе
+        иметь оригиналы: гражданского паспорта РФ и документа воинского учета,
+        для уточнения данных по воинскому учету. Тел. для связи 84997103918
+      </p>
+      <div
+        style="
+          display: flex;
+          flex-direction: row;
+          gap: 5px;
+          align-items: center;
+          align-content: center;
+        "
+      >
+        <p>[Message clipped]</p>
+        <a href="http://SBinfo.ru/1708808394" title="d2@miee.ru"
+          >Показать полностью</a
+        >
+      </div>
+    </div>
+  </body>
+</html>"""
+text = str(text)
